1e3effd31d35a5055107685648e8002941a59e70
Bonizario <gabrielbonizario98@gmail.com>
2020-06-02 14:08:28 -0300

    feat: 'create collecting point' and 'list collecting items' routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 export default app;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,55 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => ({
+    id: item.id,
+    title: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => ({
+    item_id,
+    point_id,
+  }));
+
+  await trx('point_items').insert(pointItems);
+
+  return res.json({ success: true });
 });
 
 export default routes;
