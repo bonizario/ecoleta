05a37348af3d41fe916b1405c7ce047c490607e9
Bonizario <gabrielbonizario98@gmail.com>
2020-06-02 15:13:09 -0300

    feat: 'list filtered collecting points' route

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response, response } from 'express';
 import knex from '../database/connection';
 
 export default class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  }
+
   async show(req: Request, res: Response) {
     const { id } = req.params;
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,7 +9,9 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-routes.post('/points', pointsController.create);
+
+routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
 
 export default routes;
