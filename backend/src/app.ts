e0512ab5d96e5e3b4ab3296b6263f93ab1b9858d
Bonizario <gabrielbonizario98@gmail.com>
2020-06-02 15:42:13 -0300

    feat: add cors module

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'tmp')));
