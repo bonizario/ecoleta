4a6cd314d08c0d541bb5d2ec9ea25cb446986a36
Bonizario <gabrielbonizario98@gmail.com>
2020-06-02 15:52:54 -0300

    fix: add fake image_url when creating a new collecting point

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -59,7 +59,8 @@ export default class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image:
+        'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
