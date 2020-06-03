8d7c9943ff3852c721350b8cd430ec1c0efeab1b
Bonizario <gabrielbonizario98@gmail.com>
2020-06-03 11:41:44 -0300

    feat: map interaction in the create-point form with leaflet library

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -48,6 +49,15 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
+          <Map center={[-27.2092052, -49.6401092]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-27.2092052, -49.6401092]} />
+          </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
@@ -70,6 +80,7 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
+            {}
             <li>
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
               <span>Óleo de Cozinha</span>
