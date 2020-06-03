15d2c8fb4c00d760a7886e8db40dbe09c750038c
Bonizario <gabrielbonizario98@gmail.com>
2020-06-03 12:55:09 -0300

    feat: get states and cities from the IBGE public api when changing the <select> value

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import axios from 'axios';
+import api from '../../services/api';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
+// states of arrays and objects require manual static typification
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+
+  const handleSelectUf = (event: ChangeEvent<HTMLSelectElement>) => {
+    const uf = event.target.value;
+    setSelectedUf(uf);
+  };
+
+  const handleSelectCity = (event: ChangeEvent<HTMLSelectElement>) => {
+    const city = event.target.value;
+    setSelectedCity(city);
+  };
+
+  useEffect(() => {
+    const loadItems = async () => {
+      const response = await api.get('items');
+
+      setItems(response.data);
+    };
+
+    loadItems();
+  }, []);
+
+  useEffect(() => {
+    const loadUfs = async () => {
+      const response = await axios.get<IBGEUFResponse[]>(
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome'
+      );
+
+      const ufInitials = response.data.map(uf => uf.sigla);
+      setUfs(ufInitials);
+    };
+
+    loadUfs();
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === '0') return;
+
+    const loadCities = async () => {
+      const response = await axios.get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios?orderBy=nome`
+      );
+      const cityNames = response.data.map(city => city.nome);
+      setCities(cityNames);
+    };
+
+    loadCities();
+  }, [selectedUf]);
+
   return (
     <div id="page-create-point">
       <header>
@@ -53,25 +124,45 @@ const CreatePoint = () => {
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-
             <Marker position={[-27.2092052, -49.6401092]} />
           </Map>
 
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
+              <select
+                name="uf"
+                id="uf"
+                onChange={handleSelectUf}
+                value={selectedUf}
+              >
                 <option value="0">Selecione uma UF</option>
+                {ufs.map(uf => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select
+                name="city"
+                id="city"
+                value={selectedCity}
+                onChange={handleSelectCity}
+              >
                 <option value="0">Selecione uma cidade</option>
+                {cities.map(city => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
         </fieldset>
+
         <fieldset>
           <legend>
             <h2>Itens de coleta</h2>
